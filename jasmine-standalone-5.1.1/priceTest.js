c41aa2e146e933dc7ef9fb5d8359e1324a7a256b
Kus <larisovkirill@gmail.com>
2024-06-30 22:33:45 +0200

    Spac 'price to decmo'

---
+++ jasmine-standalone-5.1.1/priceTest.js
@@ -0,0 +1,16 @@
+import { priceToDecmo } from '../scripts/utils/priceConvertor.js';
+
+describe('TEST SUIT: CALCULATE PRICE TO DECMO', () => {
+  it('tets 2095 to 20.95', () => {
+    expect(priceToDecmo(2095)).toEqual('20.95')
+  });
+
+
+  it('tets 2000.5 to 20.01', () => {
+    expect(priceToDecmo(2000.5)).toEqual('20.01')
+  })
+
+  it('tets 0 to 0.00', () => {
+    expect(priceToDecmo(0)).toEqual('0.00')
+  })
+});
